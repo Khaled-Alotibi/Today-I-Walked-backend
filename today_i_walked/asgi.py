3eafe7b08b39a7028cbe3dc9e0432f0ba9be4aa1
Khale Alotibi <khaled.algethime@gmail.com>
2025-10-26 14:16:29 +0300

    batman

---
+++ today_i_walked/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for today_i_walked project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'today_i_walked.settings')
+
+application = get_asgi_application()

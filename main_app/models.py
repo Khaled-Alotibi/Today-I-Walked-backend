4cb97accd5d52771cf022ea1c4aa506421b34276
Khale Alotibi <khaled.algethime@gmail.com>
2025-10-27 20:35:56 +0300

    Post model

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    image = models.ImageField(upload_to="posts/")
+    steps = models.IntegerField()
+    caption = models.TextField(blank=True, default="Today i Walked.")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.useranem} - {self.created_at}"

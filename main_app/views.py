213d6b847ebd6b228197842b0ddb0e807a3a05e7
Khale Alotibi <khaled.algethime@gmail.com>
2025-10-29 04:20:21 +0300

    added patch and delete to the ProfileDetail view

--- main_app/views.py
+++ main_app/views.py
@@ -45,6 +45,34 @@ class ProfileDetail(APIView):
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
 
+    def patch(self, request, user_id):
+        try:
+            profile = get_object_or_404(Profile, user__id=user_id)
+            serializer = ProfileSerializer(profile, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                print(serializer.data)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as err:
+            return Response(
+                {"error": str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def delete(self, request, user_id):
+        profile = get_object_or_404(Profile, user__id=user_id)
+
+        # if request.user != profile.user:
+        #     return Response(
+        #         {"error": "you only can delete your account."},
+        #         status=status.HTTP_401_UNAUTHORIZED,
+        #     )
+        profile.user.delete()
+        return Response(
+            {"detail": "User and Profile has been deleted successfully"},
+            status=status.HTTP_202_ACCEPTED,
+        )
+
 
 class PostIndex(APIView):
     def get(self, request):
